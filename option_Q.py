86afbc837a7672bbcfd6b413a345899a9258a860
MattisTrees <mattistrees@gmail.com>
2015-11-10 13:17:03 -0500

    Create option_Q.py
    The quit option

---
+++ option_Q.py
@@ -0,0 +1,21 @@
+__author__ = 'matthewjordan'
+
+from variables import *
+
+
+def option_Q():
+
+    done = killyourself()
+
+    return done
+
+
+
+
+def killyourself():
+    print "-------------------------------------------------------------------------------"
+    print "You grab a large rock and smash your own head with it."
+    print "-------------------------------------------------------------------------------"
+    print
+    done = True
+    return done
