d1aa074321baa908e154a15f2fd1c9a2a85a04fe
MattisTrees <mattistrees@gmail.com>
2015-11-10 13:04:34 -0500

    Create story_functions.py
    Functions that move the story along or are part of the storyline

---
+++ story_functions.py
@@ -0,0 +1,254 @@
+__author__ = 'matthewjordan'
+
+import time, random
+
+""" ***STORY IDEAS ***
+
+    post-apocolyptic thing maybe? Needs to have a good story/frame.
+
+        You're a car mechanic and that's why you can find a car relatively easy. You can fix them up and your assailants
+        are not nearly as car savvy as you.
+
+    maybe some kind of western theme? Then i'd have to change everything car-related. No, you'd just have to
+    change from gas to hey or some shit that the horse eats.
+
+    maybe keep the zombies? naaaaaaaaaaaaaaaah
+
+    how about a 'most dangerous game' type scenario where you've been plucked from your normal life by some rich fuck
+    and are forced to fend for yu
+
+"""
+
+def openingText():
+    """This function displays the opening text for the game, to be edited (and probably to another theme like post-apocolypse)"""
+
+    print
+    print
+    print
+    print "You're totally fucked. You've 'won' the lottery. You've been"
+    print "chosen to make a big solo hike from the deep south to"
+    print "some fort in the north, they didn't even tell you what was"
+    print "in the box. All you know is that you need to get as far away"
+    print "from here as fast as you can. Those 'things' can sense wherever"
+    print "you've been and no matter where you go to hide out they always"
+    print "seem to find you wherever you are, no matter how secluded."
+    print
+    print "You've been lucky enough to find a car recently and have"
+    print "been able to stay far enough ahead to look around and gather"
+    print "supplies. Even so, you know you only have at best two"
+    print "days before they should catch up."
+    print
+    print "You're only chance is to keep moving..."
+    print
+    print
+
+def zombieMovement(hours):
+    """This function will take hours and give a random zombie speed per hour that passes"""
+
+    zombie_movement = 0
+
+    while hours > 0:
+
+        zombie_spd = random.randrange(4, 11)
+        zombie_movement += zombie_spd
+        hours -= 1
+
+    return zombie_movement
+
+def pretenseText():
+    print
+    print
+    print "This game is probably not going to be about zombies in the end, but for now it works."
+    print
+    time.sleep(6)
+    print
+    print "Anyway, lets continue:"
+    time.sleep(5)
+    print
+    print "Play the game by making decisions at every juncture, keep an eye on your inventory"
+    print "as well as your status. You will get thirsty, hungry, and tired. If any of these"
+    print "parameters reaches a certain point, you will die."
+    time.sleep(12)
+    print
+    print "In this version you start off with a car so keep in mind that you need to take care of it"
+    print "in order to succeed. Other than that, just make the best decision you can and let me know"
+    print "what you think. I need feedback of many kinds. So if you think something is unfair or"
+    print "unrealistic please let me know."
+    time.sleep(12)
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print "                                                                Above all else have fun and try to win."  # *** WOULDN'T IT BE COOL IF THERE WAS A SCARY ASCII PICTURE RIGHT ABUVE THIS??
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    time.sleep(6)
+
+    raw_input("Press any key to continue...")
+    print
+    print "ready?"
+    time.sleep(2)
+
+    print "3"
+    time.sleep(1)
+    print "2"
+    time.sleep(1)
+    print "1"
+    time.sleep(1)
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+
+def dateCall(day, hour_of_day):
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print
+    print "                                                                                 Day", day
+    time.sleep(3)
+    print "                                                                             ",hour_of_day
+    time.sleep(3)
+    print
+    print
+    print
+    print
+    print
+    print
+    print
